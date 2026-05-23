aa5941764e305618554e986d390728040e184459
xNoflo <noflo1101@gmail.com>
2026-05-22 20:01:04 -0700

    Relic UI polish
    Added description of relic when hovering over the icon after the player selected it and resumed play

--- Assets/Scripts/UI/RelicUI.cs
+++ Assets/Scripts/UI/RelicUI.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class RelicUI : MonoBehaviour
+public class RelicUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public PlayerController player;
     public int index;
@@ -11,8 +12,12 @@ public class RelicUI : MonoBehaviour
     public GameObject highlight;
     public TextMeshProUGUI label;
 
+    Image tooltipBackground;
+    bool isHovered;
+
     void Start()
     {
+        ConfigureTooltip();
         Refresh();
     }
 
@@ -36,5 +41,73 @@ public class RelicUI : MonoBehaviour
 
         if (highlight != null)
             highlight.SetActive(relic.IsActive());
+
+        SetTooltipVisible(isHovered);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovered = true;
+        SetTooltipVisible(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovered = false;
+        SetTooltipVisible(false);
+    }
+
+    void ConfigureTooltip()
+    {
+        if (label == null)
+        {
+            return;
+        }
+
+        RectTransform labelRect = label.GetComponent<RectTransform>();
+        labelRect.anchorMin = labelRect.anchorMax = labelRect.pivot = new Vector2(0.5f, 1f);
+        labelRect.anchoredPosition = new Vector2(0, -28);
+        labelRect.sizeDelta = new Vector2(170, 84);
+
+        label.fontSize = 12;
+        label.enableAutoSizing = true;
+        label.fontSizeMin = 9;
+        label.fontSizeMax = 12;
+        label.alignment = TextAlignmentOptions.TopLeft;
+        label.textWrappingMode = TextWrappingModes.Normal;
+        label.overflowMode = TextOverflowModes.Overflow;
+        label.margin = new Vector4(8, 6, 8, 6);
+        label.raycastTarget = false;
+
+        if (tooltipBackground == null)
+        {
+            GameObject backgroundObject = new GameObject("TooltipBackground");
+            backgroundObject.transform.SetParent(transform, false);
+            backgroundObject.transform.SetSiblingIndex(label.transform.GetSiblingIndex());
+
+            tooltipBackground = backgroundObject.AddComponent<Image>();
+            tooltipBackground.color = new Color(0.95f, 0.91f, 0.78f, 0.96f);
+            tooltipBackground.raycastTarget = false;
+
+            RectTransform backgroundRect = tooltipBackground.rectTransform;
+            backgroundRect.anchorMin = backgroundRect.anchorMax = backgroundRect.pivot = new Vector2(0.5f, 1f);
+            backgroundRect.anchoredPosition = labelRect.anchoredPosition;
+            backgroundRect.sizeDelta = labelRect.sizeDelta;
+        }
+
+        SetTooltipVisible(false);
+    }
+
+    void SetTooltipVisible(bool visible)
+    {
+        if (label != null)
+        {
+            label.gameObject.SetActive(visible);
+        }
+
+        if (tooltipBackground != null)
+        {
+            tooltipBackground.gameObject.SetActive(visible);
+        }
     }
 }
